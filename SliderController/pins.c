661479fd68b943c6aac0380edb657b795ebbda6a
LukasQ <LukasQ@users.noreply.github.com>
2014-05-15 15:16:27 +0200

    Create pins.c

---
+++ SliderController/pins.c
@@ -0,0 +1,45 @@
+//----- PINS -------
+//Move aka X
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
+//Gier
+#define G_STEP_PIN         60
+#define G_DIR_PIN          61
+#define G_ENABLE_PIN       56
+#define G_MIN_PIN          14
+#define G_MAX_PIN          15
+//Nick
+#define N_STEP_PIN         46
+#define N_DIR_PIN          48
+#define N_ENABLE_PIN       62
+#define N_MIN_PIN          18
+#define N_MAX_PIN          19
+//Focus
+#define F_STEP_PIN         26
+#define F_DIR_PIN          28
+#define F_ENABLE_PIN       24
+#define F_SERVO_PIN        4 // check this
+//Zoom
+#define Z_STEP_PIN         36
+#define Z_DIR_PIN          34
+#define Z_ENABLE_PIN       30
+#define Z_SERVO_PIN        5 // check this
+
+#define SDPOWER            -1
+#define SDSS               53
+#define LED_PIN            13
+
+#define CAM_PIN            11 //check This
+#define FAN_PIN            9
+#define PS_ON_PIN          12
+#define KILL_PIN           -1 //NOT AUS? -> HW Setting?
+
+/*
+#define HEATER_0_PIN       10
+ #define HEATER_1_PIN       8
+ #define TEMP_0_PIN          13   // ANALOG NUMBERING
+ #define TEMP_1_PIN          14   // ANALOG NUMBERING
+ */
